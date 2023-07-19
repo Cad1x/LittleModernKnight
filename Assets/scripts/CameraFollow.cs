2c063cefa0ebdc0508b69d0ac9f019c949de3438
Michał Cader <michcad123@gmail.com>
2023-07-19 21:05:01 +0200

    Level0
    Level0 + Dialogi + Kotek + tilemapa jungli

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // Transform gracza, za którym ma pod¹¿aæ kamera
+    public float smoothSpeed = 0.125f; // Wartoœæ opóŸnienia poruszania kamery
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        // Oblicz pocz¹tkowe przesuniêcie miêdzy kamer¹ a graczem
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        // Oblicz docelow¹ pozycjê kamery
+        Vector3 desiredPosition = target.position + offset;
+
+        // Wyg³adŸ ruch kamery, u¿ywaj¹c metody Lerp
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        // Ustaw pozycjê kamery na wyg³adzon¹ pozycjê
+        transform.position = smoothedPosition;
+    }
+}
